4b73aecc03d69f5729b7243a243219d85badfe6b
9ziggy9 <davidarogers@protonmail.com>
2023-01-26 10:10:09 -0800

    minor aesthetic changes, removed borders and added rounding

--- src/main.js
+++ src/main.js
@@ -3,30 +3,25 @@ import {Snek} from "./class.js";
 import {initButtons, initGrid} from "./init.js";
 
 function handleInput(snek) {
-  console.log(window.event.keyCode);
   switch(window.event.keyCode) {
-    // UP
   case 75: case 87: case 38:
     snek.dir = UP;
     break;
-    // DOWN
   case 74: case 83: case 40:
     snek.dir = DOWN;
     break;
-    // LEFT
   case 72: case 65: case 37:
     snek.dir = LEFT;
     break;
-    // RIGHT
   case 76: case 68: case 39:
     snek.dir = RIGHT;
     break;
   default:
-    console.log("NOT A DIRECTION!!!!");
     break;
   }
 }
 
+// TODO: MAKE ME MORE EFFICIENT
 // TIME COMPLEXITY: O(N^2)
 function clearGrid() {
   for (let row = 0; row < ROWS; row++) {
