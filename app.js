d99dbdeaa3e141ddf79a51189711f83a61fd102d
9ziggy9 <davidarogers@protonmail.com>
2023-02-02 15:31:46 -0800

    snuck in cors... sssssshhh

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const sqlite3 = require("sqlite3");
+const cors = require("cors");
 
 const PORT = process.argv[2] || 8080;
 
@@ -22,6 +23,7 @@ db.run(sql_table_score, (err) => {
 // Initializing express.
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client")));
 
